e4566142289447f0c4658c73ad044169485f63b8
Ismail-Elyousfi-P <dataengineer.elyousfi@hotmail.com>
2026-03-17 01:24:30 +0100

    Refactor: cleanup project repo & unused code

--- Models construction/predict.py
+++
@@ -1,7 +0,0 @@
-
-from music_genre_classifier.inference import predict_genre_from_file
-
-
-if __name__ == "__main__":
-    genre, prob, conf_text = predict_genre_from_file("pop.wav")
-    print(f"{conf_text}{genre} (p={prob:.2f})")
--- Models construction/test.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  5 17:22:38 2020
-
-@author: msi
-"""
-
-
